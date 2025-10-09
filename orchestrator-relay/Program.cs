45d58d2449a44608d539aa0501177183947127ca
Oscar Caravaca Martinez <53091354+ocaravaca73@users.noreply.github.com>
2025-10-09 20:56:56 +0200

    Initialize Azure Functions host with HTTP client
    Conexión con Azure (Segundo archivo)

---
+++ orchestrator-relay/Program.cs
@@ -0,0 +1,10 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var host = new HostBuilder()
+    .ConfigureFunctionsWorkerDefaults()
+    .ConfigureServices(s => { s.AddHttpClient("github"); })
+    .Build();
+
+host.Run();
